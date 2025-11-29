4d6774bdf1d5cbe548b88c3e2289e356a8e15a4c
paulpineda291-lang <paulpineda291@gmail.com>
2025-11-29 14:15:28 +0800

    Add files via upload

---
+++ DoWhile.php
@@ -0,0 +1,33 @@
+<?php
+$Bottle = 5;
+$price = 5.00;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Liquor Store</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php include 'include/header.php'; ?>
+    
+    <h1>The Liquor Store</h1>
+    <h2>Prices for Each Bottle of Beer</h2>
+    
+    <p>
+        <?php
+        do {
+            echo $Bottle;
+            echo ' Bottle cost $';
+            echo $price * $Bottle;
+            echo '<br>';
+            $Bottle--;
+        } while ($Bottle > 0);
+        ?>
+    </p>
+    
+    <?php include 'include/footer.php'; ?>
+</body>
+</html>
